7f8dbedc043393a9543a5ec02a60039fc9abb797
Bin824 <zomberrylol@gmail.com>
2025-02-05 11:29:47 -0500

    ..

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-# Name: Bin Ni
-# Email: Bin.ni81@myhunter.cuny.edu
+"""
+Name: Bin Ni
+Email: bin.ni81@myhunter.cuny.edu
+"""
 
 def my_function():
+    # Prints Hello, World!
     print("Hello, World!")
